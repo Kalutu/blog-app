f185b160a2c35acb95d6f10615f16d2effbde392
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-21 13:08:01 +0300

    Created Article model

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField()
