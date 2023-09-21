8e8cc6d2a7b6e714569c656281f4d8479fdf7da0
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-21 12:59:18 +0300

    register articles urls

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home),
     path('about/', about),
+    path('articles/', include('articles.urls')),
 ]
