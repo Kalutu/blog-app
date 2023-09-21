940e16fa4735efc15e4848c94f636691256adb72
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-21 12:21:04 +0300

    Start project

--- blogapp/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-def home(request):
-    return render(request, 'home.html')
-
-def about(request):
-    return render(request, 'about.html')
