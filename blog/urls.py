56435141aaf401768a57b17fec3cf0d0d2151a9e
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-21 11:49:20 +0300

    Set up routes

--- blog/urls.py
+++ blog/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blogapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
+    path('about/', about),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Home Page")
+
+def about(request):
+    return HttpResponse("About Page")
