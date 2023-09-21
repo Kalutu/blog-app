ecc676dba5709ef1e2d6dee11e730b36df048986
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-21 14:43:29 +0300

    Modified UI of article lisT

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    articles = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {"articles":articles})
