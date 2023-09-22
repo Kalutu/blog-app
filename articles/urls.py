f3d2ddd4c6dd1e189b4250d06e087219f3d32ca2
Kalutu Daniel <78742463+Kalutu@users.noreply.github.com>
2023-09-22 14:25:24 +0300

    Add image field

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import *
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = "articles"
 
@@ -8,3 +9,5 @@ urlpatterns = [
     path('', article_list, name='list'),
     path('<slug>/', article_detail, name='detail'),
 ]
+
+urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blogapp/settings.py
+++ blogapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL='/media/'
